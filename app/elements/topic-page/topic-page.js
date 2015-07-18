1835b6a12a0d19b0d271347732d8ec7d1909c2c8
Joyee Cheung <joyeec9h3@gmail.com>
2015-07-18 20:50:18 +0800

    finish submit

--- app/elements/topic-page/topic-page.js
+++ app/elements/topic-page/topic-page.js
@@ -52,6 +52,7 @@
       this.title = data.title;
       this.topicId = data._id;
       this.content = data.content;
+      document.querySelector('#topic-content').innerHTML = this.content;
       this.userName = data.user_name;
       this.userEmail = data.user_email;
       this.lastUpdate = data.last_update;
@@ -71,6 +72,28 @@
     loadById: function(id) {
       this.$.topicPageAjax.url = '/api/topic/' + id;
       this.$.topicPageAjax.generateRequest();
+    },
+    submitReply: function(e) {
+      var newReply = {
+        user_name: this.add_reply_user_name,
+        user_email: this.add_reply_user_email,
+        content: this.add_reply_content,
+        topic_id: this.topicId
+      }
+
+      var ajax = document.createElement('iron-ajax');
+      ajax.url = '/api/reply'
+      ajax.body = JSON.stringify(newReply);
+      ajax.method = 'POST';
+      ajax.contentType="application/json";
+      ajax.addEventListener('response', function() {
+        var toast = document.createElement('paper-toast');
+        toast.text = "提交成功！";
+        app.appendChild(toast);
+        toast.show();
+        page('/topic/' + newReply.topic_id);
+      });
+      ajax.generateRequest();
     }
   });
 })();
